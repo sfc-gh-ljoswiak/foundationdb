181fbe05cdaa0d3ef3409a40f0bbe80df690aefa
Lukas Joswiak <lukas.joswiak@snowflake.com>
2020-10-29 19:27:20 +0000

    Remove async_send and return immediately

--- flow/Net2.actor.cpp
+++ flow/Net2.actor.cpp
@@ -618,10 +618,11 @@ public:
 
 	Future<int> send(uint8_t const* begin, uint8_t const* end) override {
 		++g_net2->countUDPWrites;
-		ReadPromise p("N2_UDPWriteError", id);
-		auto res = p.getFuture();
-		socket.async_send(boost::asio::const_buffer(begin, end - begin), std::move(p));
-		return res;
+		// ReadPromise p("N2_UDPWriteError", id);
+		// auto res = p.getFuture();
+		// socket.async_send(boost::asio::const_buffer(begin, end - begin), std::move(p));
+		// return res;
+		return Future<int>(5);
 	}
 
 	Future<int> sendTo(uint8_t const* begin, uint8_t const* end, NetworkAddress const& peer) override {
